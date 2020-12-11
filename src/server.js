deef44d4b1c677b8779b65dfc277bb6c0b6114bc
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-11 09:03:45 +0000

    create simple route and utilities file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const reviewsRoutes = require("./reviews");
 
 const server = express();
 const port = 3077;
+
+server.use(express.json());
+server.use("/reviews", reviewsRoutes);
 
 server.listen(port, () => {
   console.log("Server running away on port: ", port);
