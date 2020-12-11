009c20042d3a50f5c36183c8ba991b46edb8cab3
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-11 10:07:29 +0000

    created errorhadnling and imported to server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const reviewsRoutes = require("./reviews");
+const {
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  catchAllHandler,
+} = require("./lib/errorHandling");
 
 const server = express();
 const port = 3077;
 
 server.use(express.json());
 server.use("/reviews", reviewsRoutes);
+
+server.use(notFoundHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(badRequestHandler);
+server.use(catchAllHandler);
 
 server.listen(port, () => {
   console.log("Server running away on port: ", port);
