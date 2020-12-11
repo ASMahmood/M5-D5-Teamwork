9ae98b5fc650a6921a36061c6cd579a16ee4f6a4
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-11 13:04:43 +0200

    Product methods are created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const { join } = require("path");
+const productsRouter = require("./products");
 
 const server = express();
 const port = 3077;
+
+const publicFolderPath = join(__dirname, "../public");
+
+server.use(express.json());
+server.use(express.static(publicFolderPath));
+
+server.use("/products", productsRouter);
 
 server.listen(port, () => {
   console.log("Server running away on port: ", port);
